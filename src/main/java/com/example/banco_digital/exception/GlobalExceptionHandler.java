9fec67af24a7466008127602345a8c12120e985d
Janderson Jean <jandersonjean@gmail.com>
2025-05-01 09:30:36 -0300

    Validação Básica

--- src/main/java/com/example/banco_digital/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/banco_digital/exception/GlobalExceptionHandler.java
@@ -1,9 +1,12 @@
 package com.example.banco_digital.exception;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -16,5 +19,14 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(SaldoInsuficienteException.class)
     public ResponseEntity<String> handleSaldoInsuficiente(SaldoInsuficienteException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<String> handleConstraintViolation(ConstraintViolationException ex) {
+        String mensagem = ex.getConstraintViolations().stream()
+                .map(v -> v.getPropertyPath() + ": " + v.getMessage())
+                .collect(Collectors.joining(", "));
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(mensagem);
     }
 }
